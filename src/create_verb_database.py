9c98a5e455f5e41b0db9308d7b6f73793d7568e3
rbn <drbn.rbn@tutanota.com>
2024-05-14 20:51:12 +0200

    format code with black

--- src/create_verb_database.py
+++ src/create_verb_database.py
@@ -1,45 +1,50 @@
+#!/usr/bin/env python3
+
+# pylint: disable=C0114, C0116
 import json
 import pickle
 
+
 def read_verbs() -> list[dict]:
     entries = []
-    with open("data/verb.json") as json_file:
+    with open("data/verb.json", "r", encoding="utf-8") as json_file:
         for line in json_file:
             word_entry = json.loads(line)
             entries.append(word_entry)
     return entries
 
 
-def concatenate(iterable : list, sep=',') -> str:
+def concatenate(iterable: list, sep=",") -> str:
     res = iterable[0]
     for s in iterable[1:]:
-        res += (sep + s)
+        res += sep + s
     return res
 
 
-def is_good_word(entry : dict, good_words : list[str]) -> bool:
+def is_good_word(entry: dict, good_words: list[str]) -> bool:
     if entry["word"] in good_words:
         return True
     return False
 
 
-def select_conjugation(entry : dict) -> dict[str, str]:
-    my_tags = ['indicative', 'present']
-    conjs = {'verb': entry['word'],}
-    j = 0
-    for form in entry['forms']:
-        if all([tag in form['tags'] for tag in my_tags]):
-            new_key = [t for t in form['tags'] if t not in my_tags]
+def select_conjugation(entry: dict) -> dict[str, str]:
+    my_tags = ["indicative", "present"]
+    conjs = {
+        "verb": entry["word"],
+    }
+    for form in entry["forms"]:
+        if all(tag in form["tags"] for tag in my_tags):
+            new_key = [t for t in form["tags"] if t not in my_tags]
             new_key = concatenate(new_key)
-            conjs[new_key] = form['form']
+            conjs[new_key] = form["form"]
     return conjs
 
 
-def construct_conj_table(entries : list[dict]) -> list[dict]:
+def construct_conj_table(entries: list[dict]) -> list[dict]:
     conjs = []
 
     good_words = []
-    with open("data/good_words.txt") as f:
+    with open("data/good_words.txt", encoding="utf-8") as f:
         for line in f:
             good_words.append(line.strip())
 
@@ -53,8 +58,8 @@ def create_conj_db():
     entries = read_verbs()
     conjs = construct_conj_table(entries)
     fname = "verb_dict.pyobj"
-    
-    with open("data/" + fname, 'wb') as f:
+
+    with open("data/" + fname, "wb") as f:
         pickle.dump(conjs, f)
 
 
--- src/learn_german_verbs.py
+++ src/learn_german_verbs.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
+# pylint: disable=C0114, C0116
 import pickle
 import random
-import colorama
-import readline
 import difflib
+import colorama
 
 PRONOUNS = {
     ("singular", "first-person"): "ich",
@@ -10,42 +12,43 @@ PRONOUNS = {
     ("singular", "third-person"): "er/sie/es",
     ("plural", "first-person"): "wir",
     ("plural", "second-person"): "ihr",
-    ("plural", "third-person"): "sie"
+    ("plural", "third-person"): "sie",
 }
 
+DIR_PATH = "/home/rbn/git/german_verb/"
 
-def select_pronoun(conjugation : str) -> str:
+
+def select_pronoun(conjugation: str) -> str:
     number, person = conjugation.split(",")
-    if person == "singular" or person == "plural":
+    if person in ("singular", "plural"):
         person, number = number, person
 
     return PRONOUNS[(number, person)]
 
 
 def load_verbs() -> list:
-    with open("data/verb_dict.pyobj", "rb") as file:
-        verbs = pickle.load(file)
-    return verbs
+    with open(DIR_PATH + "data/verb_dict.pyobj", "rb") as file:
+        return pickle.load(file)
 
 
-def diff_conjugation(user_input : str, correct : str) -> str:
+def diff_conjugation(user_input: str, correct: str) -> str:
     cl_save = colorama.Fore.GREEN
     cl_delete = colorama.Fore.RED
     cl_nill = colorama.Style.RESET_ALL
-    
+
     result = ""
-    
+
     differ = difflib.ndiff(user_input, correct)
     for d in differ:
-        if d[0] == ' ':
+        if d[0] == " ":
             result += cl_save + d[2] + cl_nill
-        elif d[0] == '-':
+        elif d[0] == "-":
             result += cl_delete + d[2] + cl_nill
-            
+
     return result
 
 
-def interactive_loop(verbs : list):
+def interactive_loop(verbs: list):
     cl_wrong = colorama.Fore.RED
     cl_correct = colorama.Fore.GREEN
     cl_good_word = colorama.Fore.BLUE
@@ -54,31 +57,36 @@ def interactive_loop(verbs : list):
 
     while True:
         rand_entry = random.choice(verbs)
-        rand_conjugation = random.choice([key for key in rand_entry.keys() if key != 'verb'])
+        rand_conjugation = random.choice(
+            [key for key in rand_entry.keys() if key != "verb"]
+        )
         print(f"Conjugate the verb {cl_to_do + rand_entry['verb'] + cl_nill}:")
-        print(select_pronoun(rand_conjugation), ' ', end='')
+        print(select_pronoun(rand_conjugation), " ", end="")
 
         try:
             user_input = input()
         except EOFError:
             break
 
-        if user_input == 'exit':
+        if user_input == "exit":
             break
 
         if user_input == rand_entry[rand_conjugation]:
             print(cl_correct + "Correct!" + cl_nill)
         else:
             diff_entry = diff_conjugation(user_input, rand_entry[rand_conjugation])
-            print(cl_wrong + "Wrong!" + cl_nill, 
-                  " The correct answer is", 
-                  cl_good_word + rand_entry[rand_conjugation] + cl_nill,
-                  f"not {diff_entry}")
+            print(
+                cl_wrong + "Wrong!" + cl_nill,
+                " The correct answer is",
+                cl_good_word + rand_entry[rand_conjugation] + cl_nill,
+                f"not {diff_entry}",
+            )
         print()
 
+
 if __name__ == "__main__":
-    verbs = load_verbs()
+    gverbs = load_verbs()
     print("Welcome to the German Verbs Learning Program™!")
     print("Let's practice conjugating German verbs.")
     print()
-    interactive_loop(verbs)
+    interactive_loop(gverbs)
